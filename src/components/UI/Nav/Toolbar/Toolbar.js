1c7bbd7edcc0c67e49ba44abdc56f197fc6293d3
Mitch-Tyrer <mltcinematography@gmail.com>
2019-01-04 15:54:37 -0700

    minor syntax updates

--- src/components/UI/Nav/Toolbar/Toolbar.js
+++ src/components/UI/Nav/Toolbar/Toolbar.js
@@ -3,13 +3,14 @@ import classes from './Toolbar.module.css';
 import Logo from '../../../Logo/Logo';
 import NavItems from '../NavItems/NavItems';
 
-
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div className={classes.Logo}>
             <Logo />
         </div>
-        <div onClick={props.clicked}>MENU</div>
+        <div onClick={props.clicked}>
+            Menu
+        </div>
         <nav className={classes.DesktopOnly}>
             <NavItems />
         </nav>
