1c1bb46847b3efdf8607980a8ece322bb3216901
Mitch-Tyrer <mltcinematography@gmail.com>
2019-01-04 09:36:06 -0700

    created add ingredients logic

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,7 +12,10 @@ const controls = [
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
         {controls.map(ctrl => (
-            <Control key={ctrl.label} label={ctrl.label} />
+            <Control key={ctrl.label} 
+                label={ctrl.label} 
+                added={() => props.ingredientAdded(ctrl.type)}
+            />
         ))}
     </div>
 );
--- src/components/Burger/BuildControls/Control/Control.js
+++ src/components/Burger/BuildControls/Control/Control.js
@@ -4,8 +4,8 @@ import classes from "./Control.module.css";
 const control = (props) => (
     <div className={classes.Control}>
         <div className={classes.Label}>{props.label}</div>
-        <button className={classes.Less}>+</button>
-        <button className={classes.More}>-</button>
+        <button className={classes.More} onClick={props.added}>+</button>
+        <button className={classes.Less}>-</button>
     </div>
 );
 
