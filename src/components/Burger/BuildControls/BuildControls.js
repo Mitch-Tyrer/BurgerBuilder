f2872135760ea70207558a2eaa7104e2029a6655
Mitch-Tyrer <mltcinematography@gmail.com>
2019-01-03 19:39:00 -0700

    added conrols to burgerbuilder page

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import classes from './BuildControls.module.css';
+import Control from './Control/Control';
 
-const buildControls = (props) => {
-    <div>
-        
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'}
+];
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        {controls.map(ctrl => (
+            <Control key={ctrl.label} label={ctrl.label} />
+        ))}
     </div>
-};
+);
 
 export default buildControls;
 
